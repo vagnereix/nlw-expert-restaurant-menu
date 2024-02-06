6f3a8a9e9487781e7bf787e13607d882e3644921
Vagner Reis <vagnereix.dev@gmail.com>
2024-02-06 00:35:14 -0300

    feat: add default layout with loading and header component

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/components/header';
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 pt-8'>
+      <Header title='Faça seu pedido' />
+    </View>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,31 @@
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+
+type HeaderProps = {
+  title: string;
+  cartQuantityItems?: number;
+};
+
+export function Header({ title, cartQuantityItems = 1 }: HeaderProps) {
+  return (
+    <View className='flex-row items-center border-b border-slate-700 pb-5 mx-5'>
+      <View className='flex-1'>
+        <Image source={require('@/assets/logo.png')} className='h-6 w-32' />
+        <Text className='text-white text-xl font-heading mt-2'>{title}</Text>
+      </View>
+
+      <TouchableOpacity className='relative' activeOpacity={0.7}>
+        {!!cartQuantityItems && (
+          <View className='bg-lime-300 h-4 w-4 rounded-full items-center justify-center absolute z-10 -top-1.5 -right-1.5'>
+            <Text className='text-slate-900 font-bold text-xs'>
+              {cartQuantityItems}
+            </Text>
+          </View>
+        )}
+
+        <Feather name='shopping-bag' color={colors.white} size={24} />
+      </TouchableOpacity>
+    </View>
+  );
+}
